76f03bda1a4fa82135795cb9f42587a5018a6134
Oleg Mitrofanov <it@unlim.xyz>
2015-06-15 22:31:17 -0400

    Fix bad interpreter error in linprivchecker.py

--- linprivchecker.py
+++ linprivchecker.py
@@ -1,4 +1,4 @@
-#!/usr/env python
+#!/usr/bin/env python
 
 ###############################################################################################################
 ## [Title]: linuxprivchecker.py -- a Linux Privilege Escalation Check Script
